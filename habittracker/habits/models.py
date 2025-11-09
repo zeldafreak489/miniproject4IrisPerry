5678a85f9483cc6fb8511eb68c56539e918bd9c7
Iris Perry <139735067+zeldafreak489@users.noreply.github.com>
2025-11-09 21:24:15 +0000

    Updated models to include user

--- habittracker/habits/models.py
+++ habittracker/habits/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Habit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
